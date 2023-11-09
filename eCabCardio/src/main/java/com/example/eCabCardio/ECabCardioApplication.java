d69ef3d33cedb0189158d564f45aaf98848138d2
lidialuncian <lidia.luncian15@gmail.com>
2023-11-09 13:26:01 +0200

    backend project

---
+++ eCabCardio/src/main/java/com/example/eCabCardio/ECabCardioApplication.java
@@ -0,0 +1,13 @@
+package com.example.eCabCardio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECabCardioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECabCardioApplication.class, args);
+	}
+
+}
